c2bd6678185781174a0483b66f565d78539aa70b
Michael makarov <info1117@gmail.com>
2012-03-26 15:15:28 -0700

    New

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Checkin2::Application.routes.draw do
   get "home/index"
 
-  devise_for :users
+  devise_for :users, :controllers => {:sessions => 'sessions', :registrations => 'registrations'}
 
   resources :devices
 
@@ -56,7 +56,7 @@ Checkin2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+ root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
